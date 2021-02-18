fc78f4f28e79c465db3f19e71d5eda0c9c5238cf
Sergio Otzoy <otzoy97@hotmail.com>
2021-02-17 19:50:08 -0600

    agregado bloqueo para usuario no registrados

--- Practica 1/client/src/App.js
+++ Practica 1/client/src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch
+} from "react-router-dom";
 import SignIn from "./components/signin/SignInView";
 import CoachView from "./components/coach-profile/CoachView";
 import ProfileView from "./components/user-profile/ProfileView";
@@ -7,12 +12,7 @@ import OxygenView from "./components/graphic/OxygenView";
 import TemperatureView from "./components/graphic/TemperatureView";
 import HeartView from "./components/graphic/HeartView";
 import NavBar from "./components/nav-bar/NavBarView";
-
-function setToken(userToken) {
-  sessionStorage.setItem("token", JSON.stringify(userToken));
-}
-
-function getToken() {}
+import { ProvideAuth, useAuth } from "./services/useInfo";
 
 /**
  * Vista de login
@@ -24,53 +24,74 @@ function getToken() {}
  * Metrica de temperatura (usuario y atleta) (historial y tiempo real)
  */
 function App() {
-  //const token = getToken();
-  // if (!token) {
-  //   return <SingIn setToken = {setToken}/>
-  // }
   return (
-    <Router>
-      <Switch>
-        <Route path="/signin">
-          <SignIn />
-        </Route>
-        <Route path="/coach">
-          <NavBar />
-          <CoachView />
-        </Route>
-        <Route path="/info-user">
-          <NavBar />
-          <ProfileView />
-        </Route>
-        <Route path="/dashboard">
-          <NavBar />
-          <DashboardView />
-        </Route>
-        <Route path="/dashboard/:id">
-          <NavBar />
-          <DashboardView />
-        </Route>
-        <Route path="/athlete/info/:id">
-          <NavBar />
-          <ProfileView />
-        </Route>
-        <Route path="/athlete/stats/heart/:id">
-          <NavBar />
-          <HeartView />
-        </Route>
-        <Route path="/athlete/stats/temp/:id">
-          <NavBar />
-          <TemperatureView />
-        </Route>
-        <Route path="/athlete/stats/oxygen/:id">
-          <NavBar />
-          <OxygenView />
-        </Route>
-        <Route path="/">
-          <SignIn />
-        </Route>
-      </Switch>
-    </Router>
+    <ProvideAuth>
+      <Router>
+        <Switch>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+          <PrivateRoute path="/coach">
+            <NavBar />
+            <CoachView />
+          </PrivateRoute>
+          <PrivateRoute path="/info-user">
+            <NavBar />
+            <ProfileView />
+          </PrivateRoute>
+          <PrivateRoute path="/dashboard">
+            <NavBar />
+            <DashboardView />
+          </PrivateRoute>
+          <PrivateRoute path="/dashboard/:id">
+            <NavBar />
+            <DashboardView />
+          </PrivateRoute>
+          <PrivateRoute path="/athlete/info/:id">
+            <NavBar />
+            <ProfileView />
+          </PrivateRoute>
+          <PrivateRoute path="/athlete/stats/heart/:id">
+            <NavBar />
+            <HeartView />
+          </PrivateRoute>
+          <PrivateRoute path="/athlete/stats/temp/:id">
+            <NavBar />
+            <TemperatureView />
+          </PrivateRoute>
+          <PrivateRoute path="/athlete/stats/oxygen/:id">
+            <NavBar />
+            <OxygenView />
+          </PrivateRoute>
+          <PrivateRoute path="/">
+            <NavBar />
+            <DashboardView />
+          </PrivateRoute>
+        </Switch>
+      </Router>
+    </ProvideAuth>
+  );
+}
+
+/**
+ * Crea un componente que utiliza como base <Route>
+ * Si hay un usuario logeado, permitirá el paso a la ruta 'privada'
+ * Si no hay un usuario logeado, redirigirá a la página signin
+ * @param {*} param0 
+ */
+function PrivateRoute({ children, ...rest }) {
+  let auth = useAuth();
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => {
+        return auth.user === true ? (
+          children
+        ) : (
+          <Redirect to={{ pathname: "/signin", state: { from: location } }} />
+        );
+      }}
+    />
   );
 }
 
--- Practica 1/client/src/components/signin/SignInView.js
+++ Practica 1/client/src/components/signin/SignInView.js
@@ -1,59 +1,102 @@
 import React, { useState } from "react";
-import { urlServer } from "../../config";
-import PropTypes from 'prop-types'
-import axios from 'axios';
+import { Redirect, useLocation } from "react-router-dom";
+import { useAuth } from "../../services/useInfo";
 
-async function signIn(credentials) {
-    const res = await axios.get(urlServer + `login/${credentials.username}/${credentials.password}`);
-    // const obj = eval("(" + JSON.stringify(res) + ")");
-    // const parseado = JSON.stringify(obj);
-    // const data = JSON.parse(parseado).data;
-    const data = res.data;
-    return data;
-}
+export default function SignIn() {
+  // Sirve para acceder a la función signIn de useInfo
+  const auth = useAuth();
+  const {state} = useLocation();
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [alert, setAlert] = useState();
 
-export default function SignIn(/*{setToken}*/) {
-    const [username, setUser] = useState();
-    const [password, setPass] = useState();
-
-    const handleSubmit = async e => {
-        e.preventDefault();
-        try {
-            const infoUser = await signIn({
-                username, password
-            });            
-            sessionStorage['infoUser'] = infoUser;
-            this.props.history.push('coach');
-        } catch (error) {
-        }
-    }
-
-    return (
-        <div className="container vh-100">
-            <div className="row align-items-center h-100">
-                <div className="col-1"></div>
-                <form className="col-10 border border-info rounded" onSubmit={handleSubmit}>
-                    <div className="row mt-4">
-                        <span className="col-lg-2 col-xs-0 col-sm-0"></span>
-                        <h3 className="col-lg-8 col-xs-12 col-sm-12 text-center">Iniciar sesión</h3>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="username"></label>
-                        <input onChange={e => setUser(e.target.value)} type="text" className="form-control" id="username" name="username" required placeholder="Usuario"></input>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password"></label>
-                        <input onChange={e => setPass(e.target.value)} type="password" className="form-control" id="password" name="password" required placeholder="Contraseña"></input>
-                    </div>
-                    <div className="d-grid gap-2 my-4">
-                        <button type="submit" className="btn btn-outline-primary">Iniciar sesión</button>
-                    </div>
-                </form>
+  /**
+   * Solicita al servidor la autenticación del usuario
+   * Si el usuario existe, almacena su información en localStorage
+   * Si no existe informa, muestra una alerta indicando que las credenciales son erróneas
+   * @param {*} e
+   */
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const infoUser = await auth.signIn({
+      username,
+      password,
+    });
+    if (!infoUser) {
+      // Coloca una alerta indicando que las credenciales no funcionan
+      setAlert(
+        <div className="row">
+          <div className="col">
+            <div className="alert alert-danger alert-dismissible fade show" role="alert" >
+              <strong>No se pudo iniciar sesión</strong>{" "}
+              Verifique sus credenciales
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="alert"
+                aria-label="Close"
+                onClick={setAlert()}
+              ></button>
             </div>
+          </div>
         </div>
-    );
-}
+      );
+    } else {
+      // Redirige la navegación al dashboard
+      <Redirect to='/dashboard' />
+    }
+  };
 
-//SingIn.propTypes = {
-    //setToken: PropTypes.func.isRequired
-//}
+  if (auth.user === true) {
+    return <Redirect to={state?.from || '/'} />
+  }
+
+  return (
+    <div className="container vh-100">
+      <div className="row align-items-center h-100">
+        <div className="col-1"></div>
+        <form
+          className="col-10 border border-info rounded"
+          onSubmit={handleSubmit}
+        >
+          <div className="row mt-4">
+            <span className="col-lg-2 col-xs-0 col-sm-0"></span>
+            <h3 className="col-lg-8 col-xs-12 col-sm-12 text-center">
+              Iniciar sesión
+            </h3>
+          </div>
+          {alert}
+          <div className="form-group">
+            <label htmlFor="username"></label>
+            <input
+              onChange={(e) => setUsername(e.target.value)}
+              type="text"
+              className="form-control"
+              id="username"
+              name="username"
+              required
+              placeholder="Usuario"
+            ></input>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password"></label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              required
+              placeholder="Contraseña"
+            ></input>
+          </div>
+          <div className="d-grid gap-2 my-4">
+            <button type="submit" className="btn btn-outline-primary">
+              Iniciar sesión
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
