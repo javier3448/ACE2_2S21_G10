fd0f47bdde3da17d8877209b591f2e60b08835a4
Sergio Otzoy <otzoy97@hotmail.com>
2021-02-18 02:29:16 -0600

    agregada info sobre atleta

--- Practica 1/client/src/components/coach-profile/AthleteCardView.js
+++ Practica 1/client/src/components/coach-profile/AthleteCardView.js
@@ -1,38 +1,40 @@
-import React, {useState, useEffect} from 'react'
-
-
+import React, { useState, useEffect } from "react";
+import { Link, Redirect, useLocation } from "react-router-dom";
 export default function AthleteCard(props) {
-
-    return (
-        <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-4">
-            <div className="card">
-                <div className="card-header">
-                    <div className="d-flex justify-content-between">
-                        <h4 className="card-title" >{props.data.name}</h4>
-                        <button className="btn btn-sm btn-outline-dark">
-                            <span><i className="fa fa-eye"></i></span>
-                        </button>
-                    </div>
-                </div>
-                <div className="card-body">
-                    <div className="row">
-                        <div className="col">Edad:</div>
-                        <div className="col">{props.data.url}</div>
-                    </div>
-                    <div className="row">
-                        <div className="col">Sexo:</div>
-                        <div className="col">M</div>
-                    </div>
-                    <div className="row">
-                        <div className="col">Estatura:
-                    </div>
-                        <div className="col">1.89 m.</div>
-                    </div>
-                    <div className="row">
-                        <div className="col">Peso:</div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-4">
+      <div className="card">
+        <div className="card-header">
+          <div className="d-flex justify-content-between">
+            <h4 className="card-title">
+              {props.data.nombre + " " + props.data.apellidos}
+            </h4>
+            <Link to={`/dashboard/${props.data.IdUser}`} className="btn btn-sm btn-outline-dark">
+              <span>
+                <i className="fa fa-eye"></i>
+              </span>
+            </Link>
+          </div>
         </div>
-    );
+        <div className="card-body">
+          <div className="row">
+            <div className="col">Edad:</div>
+            <div className="col"></div>
+          </div>
+          <div className="row">
+            <div className="col">Sexo:</div>
+            <div className="col"></div>
+          </div>
+          <div className="row">
+            <div className="col">Estatura:</div>
+            <div className="col">{props.data.altura} m.</div>
+          </div>
+          <div className="row">
+            <div className="col">Peso:</div>
+            <div className="col">{props.data.peso} lbs.</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
