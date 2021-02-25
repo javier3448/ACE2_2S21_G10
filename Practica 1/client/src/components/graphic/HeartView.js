1e4134546cc2b5b84b1109416f6bcc041b83eaa0
Sergio Otzoy <otzoy97@hotmail.com>
2021-02-25 04:33:39 -0600

    modificado forma de calcular promedio

--- Practica 1/client/src/components/graphic/HeartView.js
+++ Practica 1/client/src/components/graphic/HeartView.js
@@ -1,18 +1,14 @@
-import React, { useState, useEffect } from "react";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-} from "recharts";
-import { urlServer } from "../../config";
-import TimeView from "../nav-bar/TimeView";
 import axios from "axios";
+import React, { useState } from "react";
+import {
+  CartesianGrid,
+  Legend, Line, LineChart,
+  ResponsiveContainer, Tooltip, XAxis,
+  YAxis
+} from "recharts";
 import { useInterval } from "../../services/interval";
+import {urlServer} from '../../config'
+import TimeView from "../nav-bar/TimeView";
 
 export default function HeartView() {
   /// Establece un hook para la información (dataSet)
@@ -33,30 +29,34 @@ export default function HeartView() {
   useInterval(async () => {
     /// Solicita al servidor todos los datos del usuario con id: IdUser
     const infoUser = JSON.parse(localStorage.getItem("userInfo"));
-    const response = await axios.get(urlServer + `/heart-rate/report1/${infoUser.IdUser}`);
+    var flagInsertZero = false;
+    var lastRecord;
+    /// const response = await axios.get('http://localhost:4200/api/heart/all');
+    const response = await axios.get(urlServer + `reports/heart-rate/report1/${infoUser.IdUser}`);
     /// Servirá como referencia para determinar
     /// si insertar un cero o el valor de pulso
     /// que retornó el servidor en su último valor
     const refDate = new Date();
-    const data = response.data;
-    let flagInsertZero = false;
-    if (data !== null) {
-      /// Recupera el último dato
-      const lastRecord = data[data.length - 1];
-      /// Recupera la fecha del último dato
-      const lastDate = new Date(lastRecord.dateTime);
-      /// Muestra un mensaje
-      console.log($`Fecha cliente : ${refDate}\n fecha server: ${lastDate}\n diferencia: ${refDate - lastDate}`)
-      if (refDate - lastDate > 1100) {
-        /// Si la diferencia de tiempo es mayor a 1.1 seg, insertará un cero
+    if (response.status === 200) {
+      const data = response.data;
+      if (data !== null) {
+        /// Recupera el último dato
+        lastRecord = data[data.length - 1];
+        /// Recupera la fecha del último dato
+        const lastDate = new Date(lastRecord.dateTime);
+        /// Muestra un mensaje
+        console.log(`Fecha cliente : ${refDate}\n fecha server: ${lastDate}\n diferencia: ${refDate - lastDate}`)
+        if (refDate - lastDate > 1250) {
+          /// Si la diferencia de tiempo es mayor a 1.1 seg, insertará un cero
+          flagInsertZero = true;
+        }
+      } else {
+        /// El servidor puede devolver null cuando no hay ningún registro
+        /// del sensor de latidos para el usuario dado
+        console.log(`El servidor respondió null, no hay datos`);
+        /// El servidor no tiene dato, insertará un 0
         flagInsertZero = true;
       }
-    } else {
-      /// El servidor puede devolver null cuando no hay ningún registro
-      /// del sensor de latidos para el usuario dado
-      console.log(`El servidor respondió null, no hay datos`);
-      /// El servidor no tiene dato, insertará un 0
-      flagInsertZero = true;
     }
     /// Reordena los elementos con el objetivo
     /// que parezca que la gráfica se mueve
@@ -70,18 +70,23 @@ export default function HeartView() {
       data.pulso = data.pulso;
       return data;
     }));
-    if (dataSet.length >= 60)  {
+    if (dataSet.length >= 59)  {
+      // Elimina el primer dato
       dataSet.shift();
       setData(dataSet);
     }
     /// Inserta el nuevo dato o un cero, dependiendo del resultado de flagInsertZero
-    const newData = {name: '00s', sec:0, pulso: flagInsertZero ? 0 : data.pulso };
+    const newData = {name: '00s', sec:0, pulso: flagInsertZero ? 0 : lastRecord.pulso };
     setData(data => [...data, newData]);
+    /// Filtra la información, recuperando únicamente los que sean mayor a cero
+    const filterData = dataSet.filter(value => value.pulso > 0);
     /// Calcula el promedio de pulsaciones
-    setAvg(dataSet.reduce((total, value) => total + value.pulso,0) / dataSet.length);
+    const avgData = Math.round(filterData.reduce((total, value) => total + value.pulso,0) / filterData.length);
+    setAvg(isNan(avgData) ? 0 : avgData);
     /// Determina el color del icono del corazón
-    setColorHeart((avg < 10) ? 'text-muted' : (avg >= 10 && avg < 60) ? 'text-warning' : (avg >= 60 && avg <= 100) ? 'text-success' ? 'text-danger');
+    setColorHeart((avg < 10) ? 'text-muted' : (avg >= 10 && avg < 60) ? 'text-warning' : (avg >= 60 && avg <= 100) ? 'text-success' : 'text-danger');
   }, 1000);
+
   return (
     <div className="vh-100">
       <div className="h-100">
@@ -95,93 +100,15 @@ export default function HeartView() {
           <div className="card card-body my-4">
             <div className="row">
               <ResponsiveContainer width="100%" height={400}>
-                <LineChart
-                  width={1000}
-                  height={400}
-                  data={dataSet}
-                  margin={{
-                    top: 5,
-                    right: 30,
-                    left: 20,
-                    bottom: 5,
-                  }}
-                >
-                  <XAxis
-                    dataKey="name"
-                    domain={[
-                      '59s',
-                      '58s',
-                      '57s',
-                      '56s',
-                      '55s',
-                      '54s',
-                      '53s',
-                      '52s',
-                      '51s',
-                      '50s',
-                      '49s',
-                      '48s',
-                      '47s',
-                      '46s',
-                      '45s',
-                      '44s',
-                      '43s',
-                      '42s',
-                      '41s',
-                      '40s',
-                      '39s',
-                      '38s',
-                      '37s',
-                      '36s',
-                      '35s',
-                      '34s',
-                      '33s',
-                      '32s',
-                      '31s',
-                      '30s',
-                      '29s',
-                      '28s',
-                      '27s',
-                      '26s',
-                      '25s',
-                      '24s',
-                      '23s',
-                      '22s',
-                      '21s',
-                      '20s',
-                      '19s',
-                      '18s',
-                      '17s',
-                      '16s',
-                      '15s',
-                      '14s',
-                      '13s',
-                      '12s',
-                      '11s',
-                      '10s',
-                      '09s',
-                      '08s',
-                      '07s',
-                      '06s',
-                      '05s',
-                      '04s',
-                      '03s',
-                      '02s',
-                      '01s',
-                      '00s'
-                    ]}
-                  />
+                <LineChart width={1000} height={400} data={dataSet}
+                  margin={{ top: 5, right: 30, left: 20,bottom: 5,}}>
+                  <XAxis dataKey="name"/>
                   <CartesianGrid strokeDasharray="2 2" />
                   <YAxis />
                   <Tooltip />
                   <Legend />
-                  <Line
-                    isAnimationActive={false}
-                    type="monotone"
-                    dataKey="pulso"
-                    stroke="#FF0000"
-                    activeDot={{ r: 1 }}
-                  />
+                  <Line isAnimationActive={false} type="monotone"
+                    dataKey="pulso" stroke="#FF0000" activeDot={{ r: 1 }} />
                 </LineChart>
               </ResponsiveContainer>
             </div>
