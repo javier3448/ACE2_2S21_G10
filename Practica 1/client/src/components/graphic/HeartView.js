b65041ae5aff93a4c7cfc48a9e539117220cb638
Sergio Otzoy <otzoy97@hotmail.com>
2021-02-26 23:06:25 -0600

    corregido nombre de dataKey

--- Practica 1/client/src/components/graphic/HeartView.js
+++ Practica 1/client/src/components/graphic/HeartView.js
@@ -93,7 +93,7 @@ export default function HeartView() {
               <ResponsiveContainer width="100%" height={400}>
                 <LineChart width={1000} height={400} data={dataSet}
                   margin={{ top: 5, right: 30, left: 20,bottom: 5,}}>
-                  <XAxis dataKey="timestamp"/>
+                  <XAxis dataKey="name"/>
                   <CartesianGrid strokeDasharray="2 2" />
                   <YAxis />
                   <Tooltip />
