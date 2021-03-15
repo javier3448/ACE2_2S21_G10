12597d7e44e0d38167a6b7f6cb213d94b2618676
javier <javier3448.ja@gmail.com>
2021-03-14 23:15:17 -0600

    Primer commit Proyecto 1

---
+++ Proyecto 1/arduino/Proyecto1Sketch/Proyecto1Sketch.ino
@@ -0,0 +1,14 @@
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
+}
+
+
+void loop()
+{
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(100);
+}
