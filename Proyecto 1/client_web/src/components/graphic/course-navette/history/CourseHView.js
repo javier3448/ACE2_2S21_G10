798f67f8f5629ba9b1e8232fba24bd27a9b15cfb
Sergio Otzoy <otzoy97@hotmail.com>
2021-04-10 02:24:00 -0600

    agregado animación para 3 reportes

--- Proyecto 1/client_web/src/components/graphic/course-navette/history/CourseHView.js
+++ Proyecto 1/client_web/src/components/graphic/course-navette/history/CourseHView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import TimeView from '../../../nav-bar/TimeView';
 import WeekHView from './WeekHView';
@@ -10,6 +10,63 @@ import SLapHGraph from './SLapHGraph';
 export default function CourseHView() {
   const params = useParams();
   const id = params.id;
+  const [sh1, setSh1] = useState(false);
+  const [sh2, setSh2] = useState(false);
+  const [sh3, setSh3] = useState(false);
+  const [colLg, setcolLg] = useState(4);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    switch (e.target.id) {
+      case "btn1":
+        if (!sh2 && !sh3) {
+          /// Estan visibles
+          setSh1(false);
+          setSh2(true);
+          setSh3(true);
+          setcolLg(12);
+        } else {
+          /// Estan invisibles
+          setSh1(false);
+          setSh2(false);
+          setSh3(false);
+          setcolLg(4);
+        }
+        break;
+      case "btn2":
+        if (!sh1 && !sh3) {
+          /// Estan visibles
+          setSh1(true);
+          setSh2(false);
+          setSh3(true);
+          setcolLg(12);
+        } else {
+          /// Estan invisibles
+          setSh1(false);
+          setSh2(false);
+          setSh3(false);
+          setcolLg(4);
+        }
+        break;
+      case "btn3":
+        if (!sh1 && !sh2) {
+          /// Estan visibles
+          setSh1(true);
+          setSh2(true);
+          setSh3(false);
+          setcolLg(12);
+        } else {
+          /// Estan invisibles
+          setSh1(false);
+          setSh2(false);
+          setSh3(false);
+          setcolLg(4);
+        }
+        break;
+    }
+  }
+
+  const [colLg1, setcolLg1] = useState(4);
   return (
     <div className="vh-100">
       <div className="h-100">
@@ -22,9 +79,13 @@ export default function CourseHView() {
           </div>
           <hr />
           <div className="row">
-            <div className="col-lg-4 col-md-12 col-sm-12 col-xs-12 mb-4">
+            <div className={`col-lg-${colLg} col-md-12 col-sm-12 col-xs-12 mb-4`} hidden={sh1}>
               <div className="card">
-                <div className="card-header h4 text-center">Repeticiones por fecha</div>
+                <button className="btn btn-secondary" type="button" onClick={handleClick}>
+                  <div className="h4 text-center" id="btn1">
+                    Repeticiones por fecha
+                  </div>
+                </button>
                 <div className="card-body">
                   <div className="row">
                     <div className="col">
@@ -37,9 +98,13 @@ export default function CourseHView() {
                 </div>
               </div>
             </div>
-            <div className="col-lg-4 col-md-12 col-sm-12 col-xs-12 mb-4">
+            <div className={`col-lg-${colLg} col-md-12 col-sm-12 col-xs-12 mb-4`} hidden={sh2}>
               <div className="card">
-                <div className="card-header h4 text-center">Repeticiones antes de fallar</div>
+                <button className="btn btn-secondary" type="button" onClick={handleClick}>
+                  <div className="h4 text-center" id="btn2" >
+                    Repeticiones antes de fallar
+                  </div>
+                </button>
                 <div className="card-body">
                   <div className="row">
                     <div className="col">
@@ -49,9 +114,13 @@ export default function CourseHView() {
                 </div>
               </div>
             </div>
-            <div className="col-lg-4 col-md-12 col-sm-12 col-xs-12 mb-4">
+            <div className={`col-lg-${colLg} col-md-12 col-sm-12 col-xs-12 mb-4`} hidden={sh3}>
               <div className="card">
-                <div className="card-header h4 text-center">Repeticiones antes de rendirse</div>
+                <button className="btn btn-secondary" type="button" onClick={handleClick}>
+                  <div className="h4 text-center" id="btn3">
+                    Repeticiones antes de rendirse
+                  </div>
+                </button>
                 <div className="card-body">
                   <div className="row">
                     <div className="col">
@@ -91,9 +160,6 @@ export default function CourseHView() {
                 </div>
               </div>
             </div>
-          </div>
-          <div className="row">
-
           </div>
         </div>
       </div>
