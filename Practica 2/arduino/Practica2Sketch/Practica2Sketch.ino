39760d1e11fbc7f96b78d7696d156f9281b63644
javier <javier3448.ja@gmail.com>
2021-04-21 22:53:33 -0600

    Agregar direccionamiento de flujo 'aceptable' :/

--- Practica 2/arduino/Practica2Sketch/Practica2Sketch.ino
+++ Practica 2/arduino/Practica2Sketch/Practica2Sketch.ino
@@ -16,6 +16,9 @@ SimpleEvents simpleEvents = SimpleEvents();
 // {
 // }
 
+// @TODO!!: Ponerlo adentro del setup
+long lastTime;
+
 void setup()
 {
     Serial.begin(9600);
@@ -29,16 +32,45 @@ void setup()
 
     // setup sensdor flujo YFS-201
     Yfs201::setup();
+    lastTime = millis();
 
     // simpleEvents.addEvent(0, 100, printYfs);
 }
+
+// @TODO!!: Ponerlo adentro del setup
+// Exhalar: false
+bool direccion = false;
+bool allowFlowChange = false;
+float volumenEnPulmones = 0;
 
 void loop()
 {
     Yfs201::loop();
 
     //Serial.print("caudal: ");
-    Serial.println(Yfs201::caudal_L_m, 4);
+    Serial.println(Yfs201::caudal_L_m * (direccion ? 1 : -1), 4);
 
     // simpleEvents.loop();
+    // @TODO TUNNING: los 
+    // Para determinar la direccioin del flujo no nos queda de otra mas que empezar
+    // en un flujo conocido (exhalar) y cambiar de direccion cada vez que el flujo
+    // sea 0, pero cuando sea 0 por varias 'iteraciones' de este loop() no queremos
+    // que cambie a cada rato. 
+    // Entonces se implemento algo parecido a un schmitt trigger. Que va a cambiar
+    // el flujo solo cuando el flujo baje de cierto umbral(bajo, 0.01) y haya pasado en una
+    // iteracion anterior otro umbral (alto, 0.05)
+    // tambie le llaman: HYSTERESIS
+    if(Yfs201::caudal_L_m > 0.4){
+        allowFlowChange = true;
+    }
+    if(Yfs201::caudal_L_m < 0.4 && allowFlowChange){
+        direccion = direccion ? false : true;
+        allowFlowChange = false;
+    }
+
+    // @TODO: Sacar con delta time y todo eso
+    // if(direccion)
+    //     volumenEnPulmones += currVolumen;
+    // else
+    //     volumenEnPulmones -= currVolumen;
 }
