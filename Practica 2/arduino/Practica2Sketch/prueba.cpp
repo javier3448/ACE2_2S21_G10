6cd5e0d48dfe037cd828e327c6cc24d3349c5b9d
Oscar Perez <oscar.6293@gmail.com>
2021-04-21 22:54:41 -0600

    prueba

--- Practica 2/arduino/Practica2Sketch/prueba.cpp
+++ Practica 2/arduino/Practica2Sketch/prueba.cpp
@@ -1,1 +1,16 @@
+#include "prueba.h"
 
+#include "simpleevents.h"
+#include "util.h"
+#include "yfs201.h"
+
+
+
+void Prueba::loop()
+{
+
+ switch(stateActual)
+    {
+    
+    }
+}
--- Practica 2/arduino/Practica2Sketch/prueba.h
+++ Practica 2/arduino/Practica2Sketch/prueba.h
@@ -1,1 +1,18 @@
 
+#ifndef _PRUEBA_h
+#define _PRUEBA_h
+#include "pindefs.h"
+
+namespace Prueba {
+
+enum State{
+        PLAY,
+        STOP
+    };
+  
+void setup();
+void loop();
+  extern State stateActual;
+
+#endif
+}
