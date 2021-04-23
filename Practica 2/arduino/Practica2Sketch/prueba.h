791b4249985459149c3dc32131266c55c80c222b
Oscar Perez <oscar.6293@gmail.com>
2021-04-22 19:45:08 -0600

    Update prueba.h

--- Practica 2/arduino/Practica2Sketch/prueba.h
+++ Practica 2/arduino/Practica2Sketch/prueba.h
@@ -9,6 +9,15 @@ enum State{
         PLAY,
         STOP
     };
+
+     struct Repiticion{
+        int8_t repeticionActual;
+        int8_t direccion;
+    	float weight;
+    	float tiempo; 
+    	float volumen;
+    	float vomax;
+    };
   
 void setup();
 void loop();
