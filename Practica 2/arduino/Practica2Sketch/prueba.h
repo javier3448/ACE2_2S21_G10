fef0ec5c28a34c90c258028bae9b263291480ed7
javier <javier3448.ja@gmail.com>
2021-04-23 21:52:20 -0600

    Completar estados de prueba.h

--- Practica 2/arduino/Practica2Sketch/prueba.h
+++ Practica 2/arduino/Practica2Sketch/prueba.h
@@ -5,6 +5,7 @@
 
 #include <SoftwareSerial.h>
 
+// @TODO: cambiar nombre a PruebaVO2max
 namespace Prueba {
 
     // @TODO: describir cada estado
@@ -13,14 +14,38 @@ namespace Prueba {
         WAIT_OK,
         PLAY
     };
-    extern State stateActual;
+
+    // 300000 = 5 minutos en milis
+    // 60000 = 1 minutos en milis
+    constexpr long TOTAL_PLAY_TIME = 300000;
+    // [!] PORQUE JODIDOS: `constexpr long TOTAL_PLAY_TIME = 5*60*1000;` NO FUNCIONO 
+    // talez sea porque los int_literals son de 16 bits, eso seria de lo mas estupido
+    // sad
+    constexpr bool INHALAR = true;
+    constexpr bool EXHALAR = false;
 
     extern SoftwareSerial btSerial;
+
+    extern State stateActual;
+
     extern float peso;
+
+    extern long beginTimePlay;
+    extern long lastTimeBtSent;
+
+    extern float volumenEnPulmones;
+    extern float volumenMaximoEnPulmones;
+    extern bool direccionFlujo;
+    extern bool allowFlowChange;
+    extern long lastTimeFlowSampled;
 
     void setup();
     void loop();
 
+    // 'private'
+    float updateVolumenEnPulmones();
+    void sendBtRealTime(long playTime, float volumenEnPulmones);
+    void sendBtVo2max(float vo2max);
 }
 
 #endif
